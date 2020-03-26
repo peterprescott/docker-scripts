77e4eefcc2b79db8f963e6fa780ba216dbcd65ba
Peter Prescott <prescott.peter@gmail.com>
2020-03-26 15:28:34 +0000

    update -- absolute volume works on windows

--- gds_py.bat
+++ gds_py.bat
@@ -1,1 +1,1 @@
-docker container run --rm -ti -p 8888:8888 darribas/gds_py:4.0 bash
+docker container run --rm -ti -p 8888:8888 -v C:\Users\peterprescott\gitDrive\docker-scripts\work:/home/jovyan darribas/gds_py:4.0 bash
