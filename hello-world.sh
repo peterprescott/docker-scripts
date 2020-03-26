0e35918ebbe7b7659728e3a8580936b23ba335c0
Peter Prescott <prescott.peter@gmail.com>
2020-03-26 17:54:16 +0000

    add real docker hello-world demo

--- hello-world.sh
+++ hello-world.sh
@@ -1,1 +1,5 @@
-echo 'hello world'
+echo 'This is just bash echoing to the terminal: "hello world"'
+echo 'And this is an actual Docker container doing the same:'
+
+docker container run peterprescott/my-first-repo
+
